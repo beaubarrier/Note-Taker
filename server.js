c9afc50d216a2d814b72152e7622e85421e75507
Beau Barrier <beaubarrier@gmail.com>
2021-06-11 14:21:21 -0700

    deleted repeated code

--- server.js
+++ server.js
@@ -4,17 +4,12 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 //settp the rotes 
-require('../../../Routes/apiRoutes')(app);
-require('../../../routes/htmlRoutes')(app);
+require('./Routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
