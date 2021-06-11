75cc86e95e8b31c74bda69d14013421f1862af32
Beau Barrier <beaubarrier@gmail.com>
2021-06-11 13:58:47 -0700

    initial commit

---
+++ Routes/apiRoutes.js
@@ -0,0 +1,46 @@
+// var express = require('express')
+// var app = express()
+// var notesHTML = require('./htmlRoutes')
+const { response } = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const notesHTML = require('../db/db.json');
+
+module.exports = (app) => {
+    // console.log("API routes get request");
+    // "/api/notes" 
+    app.get('/api/notes', (req, res) => {
+        // console.log("All notes in file ", notesHTML);
+        fs.readFile('./db/db.json', "utf8", (error, response) => {
+
+            if (error) { throw error }
+
+            const exisitingNOtes = JSON.parse(response);
+            console.log(exisitingNOtes)
+
+            res.json(exisitingNOtes);
+            console.log("API routes get request");
+        });
+    });
+
+
+    // app.get('/', function (req, res) {
+    //     res.send('hello root')
+    // });
+
+    app.post('/api/notes', (req, res) => {
+        console.log("New note", req.body)
+        JSON.parse(response);
+        const reqBody = req.body;
+        fs.writeFile(notesHTML)
+        // res.send('hello notes')
+        //read JSON 
+        // create anew variable req.body 
+        // fs.writeFile()
+
+        // const postNote =  fs.readFile('./db/db.json')
+    })
+
+
+};
---
+++ public/assets/js/server.js
@@ -0,0 +1,23 @@
+const PORT = process.env.PORT || 8080;
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+//settp the rotes 
+require('../../../Routes/apiRoutes')(app);
+require('../../../routes/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
+
+
