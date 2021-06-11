7adc5a40f72d62dbc1244ec675d50ad6fbfde0fd
Beau Barrier <beaubarrier@gmail.com>
2021-06-11 14:27:28 -0700

    added routes to server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
-
+const apiRoutes = require('./Routes/apiRoutes');
+const htmlRoutes = require('./Routes/htmlRoutes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
