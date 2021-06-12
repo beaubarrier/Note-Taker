d62e3b1ac2660ec2ed457e157bb91752d80e4f70
Beau Barrier <beaubarrier@gmail.com>
2021-06-12 16:21:54 -0700

    updated get request syntax.

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,26 +1,58 @@
 
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 // need to import db. need to pull from 
 const db = require('../db/db.json');
 
+// const notes = require('/notes.html')
 // use fs functions to read and write db.json, readfileasync, (inside or outside of module.exports?)
-
-
+// router? 
+console.log(db)
 module.exports = (app) => {
     // app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
-
-    fs.readFile(db, 'utf8', (err, data) => {
+    app.get('/api/notes', (req, res) => res.json(db));
+    console.log(res + "res")
+    console.log(req + "req")
+    fs.readFile(db, JSON.stringify(db), err => {
         if (err) {
-            console.error(err)
+            console.log(err)
             return
         }
-        console.log(data)
     })
-    JSON.parse(data)
 
-    console.log(data)
-    fs.writeFile('../notes.html', data, 'utf8');
+    // app.post('/api/notes', (req, res) => {
+    //     console.log(req.body);
+    //     db.push(req.body);
+
+    //     res.json(true);
+
+    //     fs.writeFile(db, JSON.stringify(db), err => {
+    //         if (err) {
+    //             console.log(err)
+    //             return
+    //         }
+    //     })
+    // });
+
+
+
+
+
+    // fs.readFile(db, 'utf8', (err, data) => {
+    //     if (err) {
+    //         console.error(err)
+    //         return
+    //     }
+    //     console.log(data)
+    // })
+
+
+
+
+    // JSON.parse(data)
+
+    // console.log(data)
+    // fs.writeFile('../notes.html', data, 'utf8');
     // app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
     // fs.writeFile('../public/notes.html');
     // app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
