f6ce5b8b6ad9376ee650df8b9f4b624e54889968
Beau Barrier <beaubarrier@gmail.com>
2021-06-13 13:53:37 -0700

    corrected get request routes. Added post and delete requests, still troubleshooting the former two

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,13 +1,38 @@
-
-// const express = require('express');
-
 const db = require('../db/db.json');
+const fs = require('fs');
+const path = require('path');
+var uniqid = require('uniqid');
+const dbPath = path.join(__dirname, '../db/db.json');
+
+
 module.exports = (app) => {
 
     app.get('/api/notes', (req, res) => res.json(db));
 
 
+    app.post('/api/notes', (req, res) => {
+        var note = req.body;
+        note.id = uniqid();
+        db.push(note);
+        res.json(true);
+        fs.writeFile("./db/db.json", JSON.stringify(db), err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+        })
+
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        const noteId = req.params.id;
+        const noteToDelete = JSON.parse(fs.readFileSync(dbPath));
+        let filteredNotes = noteToDelete.filter(function (e) {
+            return e.id != noteId;
+        });
+        fs.writeFileSync(dbPath, JSON.stringify(filteredNotes));
+        res.json("Note deleted!")
+    })
+
 }
-
-
 
--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-
-
 const express = require('express');
-// const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 1337;
 
+const PORT = process.env.PORT || 1337;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
