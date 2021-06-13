694e50bd1cb1e965e2235d6951ef51ec18ecc4a5
Beau Barrier <beaubarrier@gmail.com>
2021-06-13 12:24:37 -0700

    tried a new method for get request

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,122 +1,13 @@
 
-const router = require('express');
-const fs = require('fs');
-// const path = require('path');
-// need to import db. need to pull from 
+// const express = require('express');
+
 const db = require('../db/db.json');
-// require('../db/db.json');
-// const notes = require('/notes.html')
-// use fs functions to read and write db.json, readfileasync, (inside or outside of module.exports?)
-// router? 
-
-
 module.exports = (app) => {
+
     app.get('/api/notes', (req, res) => res.json(db));
-    // why isnt this displaying all notes?
+
+
+}
 
 
 
-    // JSON.stringify(db);
-
-    // fs.readFile('../db', db, err => {
-    //     if (err) {
-    //         console.log(err)
-    //         return
-    //     }
-
-    // })
-
-    // app.post('/api/notes', (req, res) => {
-    //     console.log(req.body);
-    //     db.push(req.body);
-
-    //     res.json(true);
-
-    //     fs.writeFile(db, JSON.stringify(db), err => {
-    //         if (err) {
-    //             console.log(err)
-    //             return
-    //         }
-    //     })
-    // });
-
-
-
-
-
-    // fs.readFile(db, 'utf8', (err, data) => {
-    //     if (err) {
-    //         console.error(err)
-    //         return
-    //     }
-    //     console.log(data)
-    // })
-
-
-
-
-    // JSON.parse(data)
-
-    // console.log(data)
-    // fs.writeFile('../notes.html', data, 'utf8');
-    // app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
-    // fs.writeFile('../public/notes.html');
-    // app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
-
-    // console.log("API routes get request");
-
-    // app.all('/', function (req, res, next) {
-    //     res.header("Access-Control-Allow-Origin", "*");
-    //     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    //     next();
-    // });
-
-    // fs.readFile('./db/db.json', "utf8", (error, response) => {
-
-    //     if (error) { throw error }
-
-    //     const exisitingNotes = JSON.parse(response);
-
-    //     console.log(exisitingNotes)
-
-    //     res.json(exisitingNotes);
-    //     console.log("API routes get request");
-
-
-    // res.send(exisitingNotes);
-    // res.end();
-
-
-
-    // app.post('/notes', (req, res) => {
-    //     console.log("New note", req.body)
-    //     JSON.parse(response);
-    //     const reqBody = req.body;
-    //     fs.writeFile(notesHTML)
-    // res.send('hello notes')
-    //read JSON 
-    // create anew variable req.body 
-    // fs.writeFile()
-
-    // const postNote =  fs.readFile('./db/db.json')
-    // })
-
-
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // app.get('/', function (req, res) {
-    //     res.send('hello root')
-    // });
--- server.js
+++ server.js
@@ -1,26 +1,19 @@
-const PORT = process.env.PORT || 1337;
+
+
 const express = require('express');
+// const path = require('path');
+
 const app = express();
+const PORT = process.env.PORT || 1337;
 
-// var router = express.Router();
-// const apiRoutes = require('./routes/apiRoutes')
-// const htmlRoutes = require('./routes/htmlRoutes');
-
-require('./routes/apiRoutes')(app)
-require('./routes/htmlRoutes')(app)
-require('./db/db.json')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-
-// app.use('/', htmlRoutes);
-// app.use('/api', apiRoutes);
-
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
 });
-
-
