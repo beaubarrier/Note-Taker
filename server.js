1255b283205d47548786e9013695b888898b3f9d
Beau Barrier <beaubarrier@gmail.com>
2021-06-13 22:36:34 -0700

    deleted comments

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 1337; // Assigns port
 app.use(express.urlencoded({ extended: true })); // Needed whenever express is used.
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));// I had to add this line or the app would not work.
-// app.use(express.static(__dirname + '/db'))
 
 require('./Routes/htmlRoutes')(app);
 require('./Routes/apiRoutes')(app);
