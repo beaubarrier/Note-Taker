fcfeff5ce1ba37b45cbbf99883c24ba7fe610209
Beau Barrier <beaubarrier@gmail.com>
2021-06-13 17:06:38 -0700

    fixed POST request, updated syntax all around

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -10,36 +10,34 @@ module.exports = (app) => {
 
     app.get('/api/notes', (req, res) => {
         fs.readFile(dbPath, "utf8", (err, data) => {
-            if (err) {
-                throw err;
-            }
+            if (err) { throw err; }
             res.json(JSON.parse(data));
         });
     });
 
     app.post('/api/notes', (req, res) => {
-        var note = req.body;
-        note.id = uniqid();
-        db.push(note);
-        res.json(true);
-        fs.writeFileSync("./db/db.json", JSON.stringify(db), err => {
-            if (err) {
-                console.error(err)
-                return
-            }
-        })
+        fs.readFile(dbPath, "utf8", (err, data) => {
+            if (err) { throw err; }
+            var mySavedNotes = JSON.parse(data)
+            var note = req.body;
+            note.id = uniqid();
+            mySavedNotes.push(note);
+            res.json(true);
+            fs.writeFileSync("./db/db.json", JSON.stringify(mySavedNotes), err => {
+                if (err) {
+                    console.error(err)
+                    return
+                }
+            })
+        });
     });
 
     app.delete('/api/notes/:id', (req, res) => {
         const id = req.params.id;
-        console.log("=====" + req.params.id);
-        console.log("++++++" + id);
         const noteDelete = JSON.parse(fs.readFileSync(dbPath));
         let noteFilter = noteDelete.filter(function (e) {
             return e.id != id;
         });
-
-        // Why isnt it writing to the html???
 
         fs.writeFileSync("./db/db.json", JSON.stringify(noteFilter));
         res.json("Note deleted!");
