12c3c2c524e40224919e3e216db09c3e6c39dbe8
Beau Barrier <beaubarrier@gmail.com>
2021-07-16 21:42:52 -0700

    Added further file structure.

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express'); // Requires express
 
-const app = express(); // Assigns variable to express
+const app = express();
 
 const PORT = process.env.PORT || 1337; // Assigns port
 
-app.use(express.urlencoded({ extended: true })); // Needed whenever express is used.
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));// I had to add this line or the app would not work.
+app.use(express.static(__dirname + '/public'))
 
 require('./Routes/htmlRoutes')(app);
 require('./Routes/apiRoutes')(app);
 
-app.listen(PORT, () => { // Tells the server to listen on specified port.
+app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
 });
